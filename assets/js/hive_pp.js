73ce147abc885773f930eb8d6d0f54d45aa42aa0
emiheskey <emmanuel@projaro.com>
2019-06-09 20:54:01 +0100

    news & Dashboard update

--- assets/js/hive_pp.js
+++ assets/js/hive_pp.js
@@ -4,13 +4,13 @@ jQuery( document ).ready(function($) {
 	/*	PrettyPhoto Modal Box Script
 	/* ---------------------------------------------------------------------- */
 	
-	if($('.kode-gallery-pretty').length){
-		$(".kode-gallery-pretty:first a[data-gal^='prettyphoto']").prettyPhoto({
+	if($('.hive-gallery-pretty').length){
+		$(".hive-gallery-pretty:first a[data-gal^='prettyphoto']").prettyPhoto({
 			animation_speed: 'normal',
 			slideshow: 10000,
 			autoplay_slideshow: true
 		});
-		$(".kode-gallery-pretty:gt(0) a[data-gal^='prettyphoto']").prettyPhoto({
+		$(".hive-gallery-pretty:gt(0) a[data-gal^='prettyphoto']").prettyPhoto({
 			animation_speed: 'fast',
 			slideshow: 10000,
 			hideflash: true
@@ -22,6 +22,39 @@ jQuery( document ).ready(function($) {
 			slideshow: 10000,
 			hideflash: true
 		});
-	
+		
+		let mainNavLinks = document.querySelectorAll("nav ul li a");
+		let mainSections = document.querySelectorAll("hive-pagesection");
+		
+		let lastId;
+		let cur = [];
+		
+		// This should probably be throttled.
+		// Especially because it triggers during smooth scrolling.
+		// https://lodash.com/docs/4.17.10#throttle
+		// You could do like...
+		// window.addEventListener("scroll", () => {
+		//    _.throttle(doThatStuff, 100);
+		// });
+		// Only not doing it here to keep this Pen dependency-free.
+		
+		window.addEventListener("scroll", event => {
+		  let fromTop = window.scrollY;
+		
+		  mainNavLinks.forEach(link => {
+			let section = document.querySelector(link.hash);
+		
+			if (
+			  section.offsetTop <= fromTop &&
+			  section.offsetTop + section.offsetHeight > fromTop
+			) {
+			  link.classList.add("current");
+			} else {
+			  link.classList.remove("current");
+			}
+		  });
+		});
 });	
+
+
 	
