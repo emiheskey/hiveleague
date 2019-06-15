9d82579862c6aa23af110e2133733d88c0942d46
emiheskey <emmanuel@projaro.com>
2019-06-15 17:23:08 +0100

    Forms, grid, datatable, email template
    Made some changes to the stylesheet  - styles and themetypo. Added to functions.js

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -527,4 +527,7 @@ if($('.slider-nav').length){
 			}
 		  }
 		}
-		
+		$(document).ready(function() {
+			$('#list').click(function(event){event.preventDefault();$('#products .item').addClass('list-group-item');});
+			$('#grid').click(function(event){event.preventDefault();$('#products .item').removeClass('list-group-item');$('#products .item').addClass('grid-group-item');});
+		});
